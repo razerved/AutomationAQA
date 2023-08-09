4d68fca8588f64328f559bc187c2f3677c9bd453
IvanMedvedev <idmedvedev@vtb.ru>
2023-08-09 21:10:13 +0300

    Домашняя работа ciCd

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -8,34 +8,13 @@ import utils.configuration.ReadProperties;
 public class LoginTest extends BaseTest {
     @Test
     public void successLoginTest() {
-        Assert.assertFalse(
+        Assert.assertTrue(
                 loginStep.successLogin(
                                 ReadProperties.username(),
                                 ReadProperties.password()
                         )
                         .isPageOpened()
         );
-    }
-
-    @Test
-    public void incorrectEmailLoginTest() {
-        Assert.assertEquals(
-                loginStep.negativeLogin("sdsd", ReadProperties.password()).getErrorTextLocator().getText(),
-                "Email/Login or Password is incorrect. Please try again.",
-                "Неверное сообщение об ошибке");
-    }
-
-    @Test
-    public void incorrectPswLoginTest() {
-        Assert.assertEquals(
-                loginStep.negativeLogin(ReadProperties.username(), "123").getErrorFieldTextLocator().getText(),
-                "Email/Login or Password is incorrect. Please try again.",
-                "Неверное сообщение об ошибке");
-    }
-
-    @Test
-    public void screenShotTest() {
-        Assert.assertTrue(false);
     }
 
 }
