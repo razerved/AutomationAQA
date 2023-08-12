ec05e55aca1724be804929ad4be6efe318646e89
IvanMedvedev <idmedvedev@vtb.ru>
2023-08-13 01:39:22 +0300

    Класная работа lombok and logger

--- src/test/java/helper/DataHelper.java
+++ src/test/java/helper/DataHelper.java
@@ -1,15 +1,19 @@
 package helper;
 
 import models.User;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import tests.LoggerTest;
 import utils.configuration.ReadProperties;
 
 public class DataHelper {
-
+    static Logger logger = LogManager.getLogger(DataHelper.class);
     public static User getAdminUser() {
         User user = new User();
         user.setEmail(ReadProperties.username());
         user.setPassword(ReadProperties.password());
 
+        logger.info(user.toString());
         return user;
     }
 
--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -1,6 +1,7 @@
 package tests;
 
 import baseEntities.BaseTest;
+import models.User;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import utils.configuration.ReadProperties;
@@ -15,5 +16,29 @@ public class LoginTest extends BaseTest {
                         .isPageOpened()
         );
     }
+   /* @Test
+    public void incorrectEmailLoginTest() {
+        User user = new User();
+        user.setEmail("asdasd");
+        user.setPassword(ReadProperties.password());
+
+        Assert.assertEquals(
+                loginStep.negativeLogin(user).getErrorTextElement().getText(),
+                "Email/Login or Password is incorrect. Please try again."
+        );
+    }
+
+    @Test
+    public void incorrectPswLoginTest() {
+        User user = new User();
+        user.setEmail(ReadProperties.username());
+        user.setPassword("123456");
+
+        Assert.assertEquals(
+                loginStep.negativeLogin(user).getErrorTextElement().getText(),
+                "Email/Login or Password is incorrect. Please try again.",
+                "Неверное сообщение об ошибке");
+    }*/
+
 
 }
