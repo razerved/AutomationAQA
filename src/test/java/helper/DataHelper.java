9cdddd2e4d3151bb50b6e59af73b69040c2175bf
IvanMedvedev <idmedvedev@vtb.ru>
2023-08-13 00:24:22 +0300

    Класная работа lombok

--- src/test/java/helper/DataHelper.java
+++ src/test/java/helper/DataHelper.java
@@ -1,11 +1,10 @@
 package helper;
 
-import models.Project;
 import models.User;
-import models.UserBuilder;
 import utils.configuration.ReadProperties;
 
 public class DataHelper {
+
     public static User getAdminUser() {
         User user = new User();
         user.setEmail(ReadProperties.username());
@@ -20,22 +19,5 @@ public class DataHelper {
         user.setPassword(ReadProperties.password());
 
         return user;
-    }
-
-   public static Project getCommonProject(){
-        Project project = new Project.Builder()
-                .withProjectName("name")
-                .withAnnouncement("jsjx")
-                .withProjectType(25)
-                .build();
-        return project;
-    }
-
-    public static UserBuilder getBuilderUser(){
-        UserBuilder newBuildUser = new UserBuilder.Builder()
-                .withUserEmail(ReadProperties.username())
-                .withUserPassword(ReadProperties.password())
-                .build();
-        return newBuildUser;
     }
 }
--- src/test/java/models/User.java
+++ src/test/java/models/User.java
@@ -1,91 +1,32 @@
 package models;
 
-import java.util.Objects;
+import lombok.*;
 
+/*@Getter
+@Setter
+@ToString
+@EqualsAndHashCode*/
+@Data
+@AllArgsConstructor
+@RequiredArgsConstructor
 public class User {
+    @EqualsAndHashCode.Exclude
+    private int id;
+
+    @NonNull
     private String username;
+
+    @NonNull @With(AccessLevel.PROTECTED)
     private String password;
+
+    //@ToString.Exclude
     private String email;
+
+    //@ToString.Exclude
     private String code;
 
-    public User() {
-    }
+    /*public User() {
 
-    public String getUsername() {
-        return username;
-    }
-    public void setUsername(String username) {
-        this.username = username;
-    }
-    public String getPassword() {
-        return password;
-    }
-    public void setPassword(String password) {
-        this.password = password;
-    }
-    public String getEmail() {
-        return email;
-    }
-    public void setEmail(String email) {
-        this.email = email;
-    }
-    public String getCode() {
-        return code;
-    }
-
-    public void setCode(String code) {
-        this.code = code;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        User user = (User) o;
-        return Objects.equals(username, user.username) && Objects.equals(password, user.password) && Objects.equals(email, user.email);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(username, password, email);
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "username='" + username + '\'' +
-                ", password='" + password + '\'' +
-                ", email='" + email + '\'' +
-                '}';
-    }
-
-
-    public static class Builder{
-        private User newUser;
-
-        public Builder(){
-            this.newUser = newUser;
-        }
-
-
-        public Builder withUserName(String userName){
-            newUser.username = userName;
-            return this;
-        }
-        public Builder withUserPassword(String userPassword){
-            newUser.password = userPassword;
-            return this;
-        }
-
-        public Builder withUserEmail(String userEmail){
-            newUser.email = userEmail;
-            return this;
-        }
-
-
-        public User build() {
-            return newUser;
-        }
-    }
+    }*/
 
 }
